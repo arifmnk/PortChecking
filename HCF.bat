652a1bd98f2f50b1a499dd5caeab234efe62ae7c
flux <ariffly2210@gmail.com>
2018-03-31 15:21:54 +0800

    Add script

---
+++ HCF.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+PowerShell.exe -windowstyle hidden -Command "& '%~dpn0.ps1'"
+PAUSE
---
+++ HCF.ps1
@@ -0,0 +1,55 @@
+﻿function Test-TCPPort
+{
+    param
+    (
+        [string[]]$ComputerName = $env:COMPUTERNAME,
+        [int[]]$TCPPort
+    )
+    foreach($C in $ComputerName)
+    {
+        $isPingable = Test-Connection -ComputerName $C -Count 1 -Quiet
+        if($isPingable)
+        {
+            foreach($port in $TCPPort)
+            {
+                $TCPTestSucceeded = $null
+                try
+                {
+                    $tcpClient = New-Object -TypeName System.Net.Sockets.TcpClient -ArgumentList $C, $Port
+                    $TCPTestSucceeded = $tcpClient.Connected
+                }
+                catch
+                {
+                    $TCPTestSucceeded = $false
+                }
+                finally
+                {
+                    $tcpClient.Dispose()
+                }
+
+                $obj = [pscustomobject]@{
+                    ComputerName = $C
+                    Port = $Port
+                    TCPTestSuceeded = $TCPTestSucceeded
+                }
+                $obj
+            }
+        }
+        else
+        {
+	    #If host cannot be accessed
+            #Write-Warning -Message "ComputerName '$C' is not pingable"
+            $TCPTestSucceeded = 'Host Unreachable'
+            $obj = [pscustomobject]@{
+                    ComputerName = $C
+                    Port = 'N/A'
+                    TCPTestSuceeded = $TCPTestSucceeded
+            }
+            $obj
+        }
+    }    
+}
+
+$servers = (import-csv -path "Insert target path here (e.g: 'C:\serverlist.csv')").Name
+Test-TCPPort -ComputerName $servers -TCPPort 80,135,445 <#|
+Export-Csv -Path "Insert output path here (e.g: 'D:\HealthCheck\MainScript\HCF.csv')"ss -NoTypeInformation#>
